22204e5690d5156353f4b9acec09a0a2269b538a
GraysonB <replaygaming34@gmail.com>
2025-09-23 12:46:27 -0400

    Switching APIs

--- main.py
+++ main.py
@@ -7,40 +7,18 @@ import requests
 import json
 
 def main():
-    api_key = "41b3d66b6d75ef6280679769661f9a1a"
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    api_key = "98265f2c4fbe417589063907251909"
+    base_url = "http://api.weatherapi.com/v1"
+    api_call = "/current.json?"
 
     city_name = input("Enter city name: ")
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    complete_url = base_url + api_call + "key=" + api_key + "&q=" + city_name
 
     response = requests.get(complete_url)
     data = response.json()
 
+
     print(data)
-
-    if data["cod"] != "404":
-        y = data["main"]
-
-        current_temperature = y["temp"]
-       
-        current_humidity = y["humidity"]
-
-        z = data["weather"]
-
-        weather_description = z[0]["description"]
-
-        print(" Temperature (in kelvin unit) = " +
-            str(current_temperature) + 
-
-        "\n humidity (in percentage) = " +
-            str(current_humidity) +
-
-        "\n description = " +
-            str(weather_description))
-
-    else:
-        print(" City Not Found ")
-
 
 if __name__ == "__main__":
     main()
